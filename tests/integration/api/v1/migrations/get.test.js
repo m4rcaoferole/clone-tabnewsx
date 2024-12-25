7a0f9701f96a9337ed35700242e17365196b8ee8
Marcos Vinicius <marcao.vfo@gmail.com>
2024-12-25 01:20:47 +0000

    fix `prettier` style linting

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -3,16 +3,15 @@ import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
-  await database.query("drop schema public cascade; create schema public;")
-})
+  await database.query("drop schema public cascade; create schema public;");
+});
 
-test('GET to /api/v1/migrations should return 200', async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations")
+test("GET to /api/v1/migrations should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
 
   expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0)
-
- })
+  expect(responseBody.length).toBeGreaterThan(0);
+});
