66c69c1d9142f4453d62f2592b55e346369090a7
Marcos Vinicius <marcao.vfo@gmail.com>
2025-01-26 11:31:55 +0000

    style: format with `lint:prettier:fix`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -5,7 +5,7 @@ beforeAll(async () => {
 });
 
 describe("GET /api/v1/status", () => {
-  describe("Anonymous user", () => { 
+  describe("Anonymous user", () => {
     test("Retrieving current system status", async () => {
       const response = await fetch("http://localhost:3000/api/v1/status");
       expect(response.status).toBe(200);
