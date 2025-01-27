1134d1f03f45bee44db6ea66082470cc29e18603
Marcos Vinicius <marcao.vfo@gmail.com>
2025-01-27 07:21:54 +0000

    style: format with `lint:prettier:fix`

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -6,7 +6,7 @@ beforeAll(async () => {
 });
 
 describe("GET /api/v1/migrations", () => {
-  describe("Anonymous user", () => { 
+  describe("Anonymous user", () => {
     test("Retrieving pending migrations", async () => {
       const response = await fetch("http://localhost:3000/api/v1/migrations");
       expect(response.status).toBe(200);
