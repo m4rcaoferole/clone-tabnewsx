647ef2cd2faa1e8eb1e3a988af2d8953305bb2bb
Marcos Vinicius <marcao.vfo@gmail.com>
2025-07-10 07:13:07 +0000

    feat: create `user.findOneByUsername` method and `/api/v1/users/[username]` endpoint

---
+++ pages/api/v1/users/[username]/index.js
@@ -0,0 +1,15 @@
+import controller from "infra/controller.js";
+import user from "models/user";
+import { createRouter } from "next-connect";
+
+const router = createRouter();
+
+router.get(getHandler);
+
+export default router.handler(controller.errrorHandlers);
+
+async function getHandler(request, response) {
+  const username = request.query.username;
+  const userFound = await user.findOneByUsername(username);
+  response.status(200).json(userFound);
+}
