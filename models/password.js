5bb628d5983f5da3e047a2cc3983a69b006a3440
Marcos Vinicius <marcao.vfo@gmail.com>
2025-12-07 00:45:52 +0000

    style: fix prettier on `models/password.js`

--- models/password.js
+++ models/password.js
@@ -7,7 +7,7 @@ async function hash(password) {
 
 function getNumberOfRounds() {
   // return process.env.NODE_ENV === "production" ? 14 : 1
-  
+
   let rounds = 1;
 
   if (process.env.NODE_ENV === "production") {
