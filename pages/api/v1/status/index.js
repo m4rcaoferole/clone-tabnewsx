05dc0dfaca064975cde986f87eac8aabdb9e09f2
Marcos Vinicius <marcao.vfo@gmail.com>
2026-03-27 14:37:32 +0900

    fix: correct caracter `errrorHandlers`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -6,7 +6,7 @@ const router = createRouter();
 
 router.get(getHandler);
 
-export default router.handler(controller.errrorHandlers);
+export default router.handler(controller.errorHandlers);
 
 async function getHandler(request, response) {
   const updatedAt = new Date().toISOString();
